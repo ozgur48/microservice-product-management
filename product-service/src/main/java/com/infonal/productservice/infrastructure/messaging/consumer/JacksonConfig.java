d92e10cbb83801052318d13aeb48f92f3a95b606
Özgür Durak <ozgur.durak@yandex.com>
2025-12-07 00:02:15 +0300

    product-service config consumer added

---
+++ product-service/src/main/java/com/infonal/productservice/infrastructure/messaging/consumer/JacksonConfig.java
@@ -0,0 +1,15 @@
+package com.infonal.productservice.infrastructure.messaging.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule()); // <-- Bu olmazsa JSON okuyamaz!
+    }
+}
